089eed190528c264df0dd6bf22b3c40301e7cd37
aniket2s <annisharma0211@gmail.com>
2025-12-02 16:04:35 +0530

    add register controller and bug fixes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -55,7 +55,7 @@ const userSchema = new Schema(
 userSchema.pre("save", async function (next) {
     if(!this.isModified("password")) return next(); // we are doing this to check if password is modified then only we will encrypt the password again.
     
-    this.password = bcrypt.hash(this.password, 10)
+    this.password = await bcrypt.hash(this.password, 10)
     next()
 })
 
